16645c7a4a772d97f2391ca1b3a3dfb0c05151ba
uschoen <uschoen@johjoh.de>
2017-11-05 10:50:01 +0100

    init

---
+++ hmc/test.py
@@ -0,0 +1,17 @@
+'''
+Created on 05.11.2017
+
+@author: uschoen
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
