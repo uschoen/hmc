7fd8d6b6a0c7b7f4866f0b125b1e6631e5c0a458
uschoen <uschoen@johjoh.de>
2017-11-25 09:35:06 +0100

    changes from master

--- hmc/gateways/cul/cul.py
+++ hmc/gateways/cul/cul.py
@@ -3,24 +3,20 @@ Created on 05.12.2016
 
 @author: uschoen
 '''
-__version__ = "1.6"
-import threading
-from time import localtime, strftime,sleep
-from datetime import datetime
-import socket,os
-import serial
-import sys
+__version__ = "2.1"
+
+import socket,logging,serial,threading,time
+
 
 
 class sensor(threading.Thread):
     '''
     classdocs
     '''
-    def __init__(self, parms,core,logger=False):
+    def __init__(self, parms,core):
         threading.Thread.__init__(self)
         self.__core=core
-        self.__logger=logger
-        
+        self.__logger=self.logger=logging.getLogger(__name__) 
         self.__config={}
         self.__config.update(self.__loadDefaultArgs())
         self.__config.update(parms)
@@ -30,94 +26,85 @@ class sensor(threading.Thread):
         self.__read_queue=[]
         self.running=1
         self.__USBport=False
-        self.__log("info","build  "+__name__+" instance")
+        self.__log.info("build  "+__name__+" instance")
+        
     def run(self):
         try:
-            self.__log("info","%s  start"%(__name__))
+            self.__log.info("%s  start"%(__name__))
             self.__openUSB()
             self.__initCUL()        
             self.__readBudget()
             while self.running:
                 if not self.__USBport:
-                    self.__log("error","can not open usb port")
+                    self.__log.error("can not open usb port")
                     try:
                         self.__USBport.close()
-                        self.__log("error","closing USB Port %s"%(self.__config['usbport']))
+                        self.__log.error("closing USB Port %s"%(self.__config['usbport']))
                     except:
-                        self.__log("error","can not closing USB Port %s"%(self.__config['usbport']))
+                        self.__log.error("can not closing USB Port %s"%(self.__config['usbport']))
                     while self.__USBport:
                         self.__openUSB()
-                        self.__log("info","wait 2 min and try again")
-                        sleep(120)    
+                        self.__log.info("wait 2 min and try again")
+                        time.sleep(120)    
                         self.__initCUL()
                         self.__readBudget()
                 read_line = self.__readResult()
                 if read_line is not None:
                     if read_line.startswith("21  "):
                         self._pending_budget = int(read_line[3:].strip()) * 10 or 1
-                        self.__log("info","get pending budget: %sms" % self._pending_budget)
+                        self.__log.info("get pending budget: %sms" % self._pending_budget)
                     elif read_line.startswith("K"):
-                        self.__log("debug","get weather sensor response from CUL: '%s'" % read_line)
+                        self.__log.debug("get weather sensor response from CUL: '%s'" % read_line)
                         self.__decodeWeather(read_line.lstrip("K"))
                     elif read_line.startswith("F"):
-                        self.__log("debug","get FS20 response from CUL: '%s'" % read_line)
+                        self.__log.debug("get FS20 response from CUL: '%s'" % read_line)
                         self.__fs20Device(read_line.lstrip("F"))
                     else:
-                        self.__log("warning","get unkown response from CUL: '%s'" % read_line)
-                sleep(self.__config['sleeping'])
-            self.__log("warning",__name__+"  pause")
+                        self.__log.warning("get unknown response from CUL: '%s'" % read_line)
+                time.sleep(self.__config['sleeping'])
+            self.__log.warning(__name__+"  pause")
         except:
-            self.__log("error",sys.exc_info())
+            self.logger.critical("cul is stopped", exc_info=True)
+            
     def __decodeWeather(self,msg):
         try:
-            self.__log("debug","weather decode: %s leng %i" %(msg,len(msg)))
+            self.__log.debug("weather decode: %s length %i" %(msg,len(msg)))
             split_msg=list(msg)
             if int(split_msg[1])==0:
-                self.__log("debug","typ is AS3:%s"%(split_msg[1]))
+                self.__log.debug("typ is AS3:%s"%(split_msg[1]))
             elif int(split_msg[1])==1:
-                self.__log("debug","typ is AS2000, ASH2000, S2000, S2001A, S2001IA, ASH2200, S300IA:%s"%(split_msg[1]))
+                self.__log.debug("typ is AS2000, ASH2000, S2000, S2001A, S2001IA, ASH2200, S300IA:%s"%(split_msg[1]))
             elif int(split_msg[1])==2:
-                self.__log("debug","typ is S2000R:%s"%(split_msg[1]))
+                self.__log.debug("typ is S2000R:%s"%(split_msg[1]))
             elif int(split_msg[1])==3:
-                self.__log("debug","typ is S2000W:%s"%(split_msg[1]))
+                self.__log.debug("typ is S2000W:%s"%(split_msg[1]))
             elif int(split_msg[1])==4:
-                self.__log("debug","typ is S2001I, S2001ID:%s"%(split_msg[1]))
+                self.__log.debug("typ is S2001I, S2001ID:%s"%(split_msg[1]))
             elif int(split_msg[1])==5:
-                self.__log("debug","typ is S2500H:%s"%(split_msg[1]))
+                self.__log.debug("typ is S2500H:%s"%(split_msg[1]))
             elif int(split_msg[1])==6:
-                self.__log("debug","typ is Pyrano (Strahlungsleistung):%s"%(split_msg[1]))
+                self.__log.debug("typ is Pyrano (Strahlungsleistung):%s"%(split_msg[1]))
             elif int(split_msg[1])==7 and len(msg)==16:
-                self.__log("debug","typ is ks300:%s"%(split_msg[1]))
+                self.__log.debug("typ is ks300:%s"%(split_msg[1]))
                 self.__ks300Device(msg)
             else:
-                self.__log("info","typ is unkown:%s leng %i"%(split_msg[1],len(msg)))
+                self.__log.info("typ is unknown:%s length %i"%(split_msg[1],len(msg)))
         except:
-            self.__log("error","can not convert weather data")
-            self.__log("error",sys.exc_info())
-            tb = sys.exc_info()
-            for msg in tb:
-                self.__log("error","Traceback Info:%s"%(msg)) 
+            self.logger.error("can not decode weather data", exc_info=True)
     def __fs20Device(self,msg):
-        self.__log("debug","fs20 get code %s"%(msg))
+        self.__log.debug("fs20 get code %s"%(msg))
         try:
             deviceID="%s@%s.%s"%(msg[0:6],self.__config['gateway'],self.__config['host'])
             rssi=self.__rssi(int(msg[8:9],16))
-            self.__log("debug","fs20 device code is %s"%(deviceID))
+            self.__log.debug("fs20 device code is %s"%(deviceID))
             if not deviceID in  self.__core.getAllDeviceId():
-                self.__log("debug","new fs20 device: %s"%(deviceID))
+                self.__log.debug("new fs20 device: %s"%(deviceID))
                 self.__core.addDevice(self.__deviceOBJ(deviceID,'fs20',msg[6:8]))
             else:
                 self.__core.setValue(deviceID,msg[6:8])
-            #self.__core.setDeviceAttribute(deviceID,"rssi",rssi)
+            self.__core.setDeviceAttribute(deviceID,"rssi",rssi)
         except :
-                self.__log("error","can not update fs20")
-                self.__log("error",sys.exc_info())
-                tb = sys.exc_info()
-                for msg in tb:
-                    self.__log("error","Traceback Info:%s"%(msg)) 
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                self.__log("error","%s %s %s "%(exc_type, fname, exc_tb.tb_lineno))
+            self.logger.critical("can not add or update fs20 device", exc_info=True)
     
     def __deviceOBJ(self,deviceID,typ,value=0):
         tempSensor={
@@ -146,10 +133,10 @@ class sensor(threading.Thread):
         return tempSensor
         
     def __ks300Device(self,msg):
-        self.__log("debug","ks300 get code %s"%(msg))
+        self.__log.debug("ks300 get code %s"%(msg))
         try:
             if len(msg)<>16:
-                self.__log("error", "inkorect message lenght: %s"%(len(msg)))
+                self.__log.error( "inkorect message lenght: %s"%(len(msg)))
                 return
             splitMsg=list(msg)
             rssi=self.__rssi(int(splitMsg[14]+splitMsg[15],16))
@@ -193,14 +180,7 @@ class sensor(threading.Thread):
             self.__core.setValue(deviceID,value)
             self.__core.setDeviceAttribute(deviceID,"rssi",rssi)
         except :
-                self.__log("error","can not update ks300")
-                self.__log("error",sys.exc_info())
-                tb = sys.exc_info()
-                for msg in tb:
-                    self.__log("error","Traceback Info:" + str(msg)) 
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                self.__log("error","%s %s %s "%(exc_type, fname, exc_tb.tb_lineno))
+            self.logger.critical("can not update ks300", exc_info=True)
 
     def __loadDefaultArgs(self):
         args={
@@ -220,17 +200,20 @@ class sensor(threading.Thread):
         else:
             rssi=rssi/2-74
         return rssi
+    
     def shutdown(self):
-        self.__log("emergency","shutdown cul")
-        self.__closeUSB()
+        self.__log.critical("shutdown cul")
         self.running=0
-        self.__log("emergency","shutdown cul finish")
+        self.__closeUSB()
+        self.__log.critical("shutdown cul finish")
+        
     def __readBudget(self):
         self.__sendCommand("X")
+        
     def __openUSB(self):
         if not self.__USBport:
             try:
-                self.__log("info","open serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']))
+                self.__log.info("open serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']))
                 self.__USBport=serial.Serial(
                               port='/dev/ttyACM0',
                               baudrate = self.__config['baudrate'],
@@ -241,77 +224,63 @@ class sensor(threading.Thread):
                             )
                 return self.__USBport.isOpen() 
             except:
-                self.__log("error",sys.exc_info())
-                tb = sys.exc_info()
-                for msg in tb:
-                    self.__log("error","Traceback Info:%s" %(msg))
-                self.__log("warning","can not open serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']))
+                self.logger.critical("can not open serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']), exc_info=True)
                 self.__USBport=False
                 return False
         return False
+    
     def __closeUSB(self):
-        self.__log("info","close serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']))
+        self.__log.info("close serial, port:%s baud:%s timeout:%s"%(self.__config['usbport'],self.__config['baudrate'],self.__config['timeout']))
         if(self.__USBport.isOpen() == True):
             self.__USBport.close()
+            
     def __initCUL(self):
         '''
         init command for cul stick
         '''
-        self.__log("info","initCUL")
+        self.__log.info("initCUL")
         self.__sendCommand("X21")
-        sleep(0.3)        
+        time.sleep(0.3)       
+         
     def __sendCommand(self, command):
-        self.__log("info","send command:%s"%(command))
+        self.__log.info("send command:%s"%(command))
         if not self.__USBport:
-            self.__log("error","usb not open")
+            self.__log.error("usb is not open")
             while self.__openUSB():
-                sleep(120)    
+                time.sleep(120)    
             self.__initCUL()
             self.__readBudget()
         if command.startswith("Zs"):
             self.__budget = 0
         self.__USBport.write(command + "\r\n")
+        
     def __readResult(self):
         while self.__USBport.inWaiting():
             self.__pending_line.append(self.__USBport.read(1))
             if self.__pending_line[-1] == "\n":
                 completed_line = "".join(self.__pending_line[:-2])
-                self.__log("debug","received: %s" %(completed_line))
+                self.__log.debug("received: %s" %(completed_line))
                 self.__pending_line = []
                 if completed_line.startswith("Z"):
                     self.__read_queue.put(completed_line)
                 else:
                     return completed_line
-    def __log (self,level="unkown",messages="no messages"):
-        if self.__logger:
-            dt = datetime.now()
-            conf={}
-            conf['package']=__name__
-            conf['level']=level
-            conf['messages']=str(messages)
-            conf['time']=strftime("%d.%b %H:%M:%S", localtime())
-            conf['microsecond']=dt.microsecond
-            self.__logger.write(conf) 
 
     
 
 if __name__ == "__main__":
 
-    class logger(object):
-        def write(self,arg):
-            print arg['messages']
     args={
               'usbport':"/dev/ttyUSB0",
               'baudrate':"9600",
               'timeout':None,
               'sleeping':1
               }
-    loging=logger()
-    threadInstance = server(args,False,loging)
+    threadInstance = sensor(args,False)
     print("build thread")
     threadInstance.daemon = True
     threadInstance.start()
     print("start thread")
     while True:
         print("main wait 10 sec")
-        sleep(10)    
+        time.sleep(10)    
