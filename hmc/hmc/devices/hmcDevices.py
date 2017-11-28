3f7c594a174cd1fefd3901013bfc1d01b5eafcee
uschoen <uschoen@johjoh.de>
2017-11-28 22:40:06 +0100

    change all attribute names to "channel" and add new function callEvent.

--- hmc/hmc/devices/hmcDevices.py
+++ hmc/hmc/devices/hmcDevices.py
@@ -6,9 +6,8 @@ Created on 05.12.2016
 __version__="2.0"
 
 from time import time
-import json,os,copy
-import logging
-from _hashlib import new
+import json,os,logging
+
 
 
 class device(object):
@@ -22,18 +21,18 @@ class device(object):
     def __init__(self,arg,eventHandlerList,adding=False):
         self.__eventHandlerList=eventHandlerList
         self.logger=logging.getLogger(__name__)
-        self._attribute={}
+        self._channels={}
         
         try:
             packagePath=arg["package"]["value"]
-            attribute=self._loadJSON("hmc/devices/hmcDevices.json")
-            self._attribute.update(attribute['attribute'])
+            channel=self._loadJSON("hmc/devices/hmcDevices.json")
+            self._channel.update(channel['channel'])
             self._jsonPath="gateways/%s/devices/%s.json"%(packagePath.replace(".", "/"),self._name_())
-            attribute=self._loadJSON( self._jsonPath)
-            self._attribute.update(attribute['attribute'])
-            self._attribute.update(arg)
+            channel=self._loadJSON( self._jsonPath)
+            self._channel.update(channel['channel'])
+            self._channel.update(arg)
         except:
-            self.logger.error("can not load attribute, %s can not build"%(self._name_()))
+            self.logger.error("can not load channels, %s can not build"%(self._name_()))
             raise
         self.logger.debug("build %s instance"%(self._name_()))
         if adding:
@@ -44,12 +43,12 @@ class device(object):
      public function
     '''
     def delete(self):
-        self.logger.info("delete device %s"%(self._attribute['deviceID']['value']))    
+        self.logger.info("delete device %s"%(self._channel['deviceID']['value']))    
         self.logger.warning("delete device not implemnt")       
         self._ondelete_event()
     '''
     ####################
-    Attribute section
+    channel section
     ####################
     '''
             
@@ -57,71 +56,74 @@ class device(object):
     get parameter
     public function
     '''    
-    def getAllAttribute(self):
-        self.logger.debug("get all attribute for device:%s"%(self._attribute['deviceID']['value']))
-        attribute_list=[]
-        for key in self._attribute:
-            if not "hidden" in self._attribute[key]:
-                attribute_list.append(key)
-        return attribute_list
+    def getAllChannel(self):
+        self.logger.debug("get all channel for device:%s"%(self._channel['deviceID']['value']))
+        channel_list=[]
+        for key in self._channel:
+            if not "hidden" in self._channel[key]:
+                channel_list.append(key)
+        return channel_list
     '''
-    add attribute
+    add channel
     '''    
-    def addAttribute(self,attribute):
-        self.logger.debug("set sensor data")
-        if (attribute.keys()[0]) in self._attribute:
-            self.logger.error("attribute: %s exist"%(attribute))
+    def addChannel(self,channel):
+        self.logger.info("add channel %s"%(channel))
+        if (channel.keys()[0]) in self._channel:
+            self.logger.error("channel: %s is exist"%(channel))
             raise
-        self._attribute.update(attribute)   
         try:
             writeJsonVars={}
             oldJsonvar=self._loadJSON(self._jsonPath)
-            newJsonVars=oldJsonvar['attribute']
-            newJsonVars.update(attribute)
-            writeJsonVars['attribute']=newJsonVars
+            newJsonVars=oldJsonvar['channel']
+            newJsonVars.update(channel)
+            writeJsonVars['channel']=newJsonVars
             self._writeJSON(self._jsonPath,writeJsonVars)
-                            
+            self.__channel.update(channel)                  
         except:
-            self.logger.error("can not write new attribute file to %s"%(self._jsonPath), exc_info=True)
-    def ifAttributeExist(self,attribute):
-        if attribute in self._attribute:
+            self.logger.error("can not write new channel to file %s"%(self._jsonPath), exc_info=True)
+            raise
+        
+    def ifChannelExist(self,channel):
+        if channel in self._channel:
             return True
         return False
+    
     '''
-    get value of attribute
+    get value of channel
     public function
      '''    
-    def getAttributeValue(self,attribute):
-        self.logger.debug("get attribute value for:%s"%(attribute))
+    def getChannelValue(self,channel):
+        self.logger.debug("get value for channel:%s"%(channel))
         try:
-            if not attribute in self._attribute:
-                self.logger.error("can not find attribute:%s "%(attribute))
+            if not channel in self._channel:
+                self.logger.error("channel %s is not exist"%(channel))
                 raise Exception
-            return self._attribute[attribute]['value']
+            return self._channel[channel]['value']
         except:
-            raise Exception
+            self.logger.error("unknown error in getChannelValue")
+            raise 
     
     '''
-     set value of attribute
+     set value of channel
      public function
      '''    
-    def setAttributeValue(self,attribute,value):
+    def setChannelValue(self,channel,value):
         try:
-            if not attribute in self._attribute:
-                self.logger.error("attribute %s to not exist"%(attribute))
-                raise Exception
-            self.logger.debug("set attribute %s to %s"%(attribute,value))
-            oldValue=self._attribute[attribute]['value']
-            self._attribute[attribute]['value']=value
+            if not channel in self._channel:
+                self.logger.error("channel %s is not exist"%(channel))
+                raise 
+            self.logger.debug("set channel %s to %s"%(channel,value))
+            oldValue=self._channel[channel]['value']
+            self._channel[channel]['value']=value
             if oldValue<>value:
-                self._onchange_event(attribute)
-            self._onrefresh_event(attribute)
+                self._onchange_event(channel)
+            self._onrefresh_event(channel)
         except:
-            self.logger.error("can not set attribute %s to %s"%(attribute,value))
-            raise Exception
+            self.logger.error("can not set channel %s to %s"%(channel,value))
+            raise 
     
     def getConfiguration(self):
-        return self._attribute
+        return self._channel
     '''
     ####################
     Events
@@ -130,37 +132,42 @@ class device(object):
     '''
     def registerEventHandler(self,eventTyp,eventName):
         self.logger.debug("register new event handler %s for event %s"%(eventTyp,eventName))
-        if eventName in  self._attribute[eventTyp]['value']:
+        if eventName in  self._channel[eventTyp]['value']:
             self.logger.warning("event handler %s for event %s is all reddy registerd"%(eventTyp,eventName))
             return
-        self._attribute[eventTyp]['value'].append(eventName)
+        self._channel[eventTyp]['value'].append(eventName)
+    
+    def _callEvent(self,eventTyp,channel):
+        self.logger.debug("call event: %s for channel: %s for deviceID:%s"%(eventTyp,channel,self._channel['deviceID']['value']))
+        self._channel[eventTyp]['time']=int(time())
         
-    def _onchange_event(self,attribute):
-        self.logger.debug("__onchange_event: %s for attribute: %s"%(self._attribute['deviceID']['value'],attribute))
-        self._attribute['lastchange']['value']=int(time())
-        for eventName in self._attribute["onchange_event"]["value"]:
+        
+    def _onchange_event(self,channel):
+        self.logger.debug("__onchange_event: %s for channel: %s"%(self._channel['deviceID']['value'],channel))
+        self._channel['lastchange']['value']=int(time())
+        for eventName in self._channel["onchange_event"]["value"]:
             self.logger.debug("calling: %s event handler"%(eventName))
             self.__eventHandlerList[eventName].callback(self)
-    def _onrefresh_event(self,attribute):
-        self.logger.debug("__onrefresh_event: %s for attribute: %s"%(self._attribute['deviceID']['value'],attribute))
-        self._attribute['lastupdate']['value']=int(time())
-        for eventName in self._attribute["onrefresh_event"]["value"]:
+    def _onrefresh_event(self,channel):
+        self.logger.debug("__onrefresh_event: %s for channel: %s"%(self._channel['deviceID']['value'],channel))
+        self._channel['lastupdate']['value']=int(time())
+        for eventName in self._channel["onrefresh_event"]["value"]:
             self.logger.debug("calling: %s event handler"%(eventName))
             self.__eventHandlerList[eventName].callback(self)
     def _onboot_event(self):
-        self.logger.debug("__onboot_event: %s"%(self._attribute['deviceID']['value']))
-        for eventName in self._attribute["onboot_event"]["value"]:
+        self.logger.debug("__onboot_event: %s"%(self._channel['deviceID']['value']))
+        for eventName in self._channel["onboot_event"]["value"]:
             self.logger.debug("calling: %s event handler"%(eventName))
             self.__eventHandlerList[eventName].callback(self)
     def _oncreate_event(self):
-        self.logger.debug("__oncreate_event: %s"%(self._attribute['deviceID']['value']))
-        self._attribute['create']['value']=int(time())
-        for eventName in self._attribute["oncreate_event"]["value"]:
+        self.logger.debug("__oncreate_event: %s"%(self._channel['deviceID']['value']))
+        self._channel['create']['value']=int(time())
+        for eventName in self._channel["oncreate_event"]["value"]:
             self.logger.debug("calling: %s event handler"%(eventName))
             self.__eventHandlerList[eventName].callback(self)
     def _ondelete_event(self):
-        self.logger.debug("__ondelete_event: %s"%(self._attribute['deviceID']['value']))
-        for eventName in self._attribute["ondelete_event"]["value"]:
+        self.logger.debug("__ondelete_event: %s"%(self._channel['deviceID']['value']))
+        for eventName in self._channel["ondelete_event"]["value"]:
             self.logger.debug("calling: %s event handler"%(eventName))
             self.__eventHandlerList[eventName].callback(self)
             
@@ -180,7 +187,7 @@ class device(object):
             self.logger.error("error in find file: %s "%(os.path.normpath(filename)), exc_info=True)
             raise
         except:
-            self.logger.error("unkown error:", exc_info=True)
+            self.logger.error("unknown error:", exc_info=True)
             raise
                       
     def _loadJSON(self,filename):
@@ -198,7 +205,7 @@ class device(object):
             self.logger.error("error in file: %s "%(os.path.normpath(filename)), exc_info=True)
             raise
         except:
-            self.logger.error("unkown error",exc_info=True)
+            self.logger.error("unknown error",exc_info=True)
             raise 
     '''    
     
