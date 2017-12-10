59b410d6ba0519b668def266e6d81a0f10c12de0
uschoen <uschoen@johjoh.de>
2017-12-10 11:48:00 +0100

    change example to new channel and logging defaults

---
+++ hmc/gateways/example/devices/example.py
@@ -0,0 +1,14 @@
+'''
+Created on 05.12.2016
+
+@author: uschoen
+'''
+from hmc.devices.defaultDevice import device
+__version__="2.0"
+
+class device(device):
+    '''
+    classdocs
+    '''
+    def _name_(self):
+        return "example"   
