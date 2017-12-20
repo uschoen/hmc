b7b76fd3024c71e098e7a1c4e0ac8ed23af14b82
uschoen <uschoen@johjoh.de>
2017-12-20 20:36:12 +0100

    add web gateway

---
+++ hmc/gateways/web/web/__init__.py
@@ -0,0 +1,31 @@
+'''
+Created on 21.10.2017
+
+@author: uschoen
+
+
+
+'''
+import threading
+import logging
+
+__version__="3.0"
+
+class server(threading.Thread): 
+    def __init__(self, params,core):
+        threading.Thread.__init__(self)
+        self.__core=core
+        self.__args=params
+        self.logger=logging.getLogger(__name__) 
+        self.running=1
+        self.logger.debug("build  %s instance with version %s"%(__name__,__version__))
+    
+    def run(self):
+        self.logger.info("%s start"%(__name__))
+        while self.running:
+            pass
+        self.logger.info("%s stop:"%(__name__))   
+    
+    def shutdown(self):
+        self.running=0
+        self.logger.critical("stop %s thread"%(__name__))
