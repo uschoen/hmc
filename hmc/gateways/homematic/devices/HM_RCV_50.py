92717674de507a3d24f7adcd35650689c9a75182
uschoen <uschoen@johjoh.de>
2017-12-23 17:27:14 +0100

    rebuild hmc devices

---
+++ hmc/gateways/homematic/devices/HM_RCV_50.py
@@ -0,0 +1,13 @@
+'''
+Created on 23.12.2017
+@author: uschoen
+
+'''
+from hmc.devices.defaultDevice import device
+
+__version__="2.0"
+
+
+class device(device):
+    def _name_(self):
+        return "HM_RCV_50"
