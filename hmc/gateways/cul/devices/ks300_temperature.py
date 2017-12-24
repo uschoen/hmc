2e7196e022184248e6ff7c3a726a08a290454f27
uschoen <uschoen@johjoh.de>
2017-12-24 10:53:42 +0100

    add device function for ks300_temperature: Channel max24 temperature Channel min24 temperature ks300 windchill: convert windchill value to index value

--- hmc/gateways/cul/devices/ks300_temperature.py
+++ hmc/gateways/cul/devices/ks300_temperature.py
@@ -4,10 +4,104 @@ Created on 24.12.2017
 
 '''
 from hmc.devices.defaultDevice import device
+from time import time
 
-__version__="2.0"
+__version__="3.0"
 
 
 class device(device):
     def _name_(self):
         return "ks300_temperature"
+    
+    def setChannelValue(self,channelName,value):
+        '''
+        set value of channel
+        ''' 
+        channelName=str(channelName)
+        channelName=channelName.lower()
+        try:
+            if not channelName in self._channels:
+                self.logger.error("channel %s is not exist"%(channelName))
+                raise Exception
+            self.logger.debug("set channel %s to %s"%(channelName,value))
+            oldValue=self._channels[channelName]['value']['value']
+            self._channels[channelName]['value']['value']=value
+            if channelName=='lasttemperature':
+                return
+            if oldValue<>value:
+                self._callEvent('onchange_event',channelName)
+                if channelName=='temperature':
+                    self.calcTemperature(value)
+            self._callEvent('onrefresh_event',channelName)
+        except:
+            self.logger.error("can not set channel %s to %s"%(channelName,value))
+            raise Exception
+    
+    def _addSysChannels(self):
+        '''
+        check if sys channel add, if channel not exists
+        add channel to device
+        
+        raise exception
+        '''
+        try:
+            sysChannels=['tempMin24h','tempMax24h','lasttemperature']
+            for channelName in sysChannels:
+                if not self.ifChannelExist(channelName):
+                    self.logger.info("add new channel %s"%(channelName))
+                    self.addChannel(channelName,self._defaultChannel(channelName))
+        except:
+            self.logger.error("can not add sys channel to temperature device")
+            raise       
+    
+    def _defaultChannel(self,channelName):
+        '''
+        return a dic with sys channel values
+        '''
+        channelValues={}
+        channelValues[channelName]={
+                "value":{
+                        "value":{},
+                        "type":"array"},
+                "name":{        
+                        "value":channelName,
+                        "type":"string"},
+                    }
+        return channelValues
+    
+    def calcTemperature(self,temperature):
+        '''
+        calc the temperature for last 24h
+        last temperature
+        min temperature 24h
+        max temperature 24h
+        '''
+        return
+        self.logger.debug("calc temperature data:%s"%(temperature))
+        lastTemperature={}
+        try:
+            self._addSysChannels()
+            self.__clearOldTemp()
+            lastTemperature=self.getChannelValue('lasttemperature')
+            lastTemperature[int(time())]=temperature
+            self.setChannelValue('lasttemperature',lastTemperature)
+            self.setChannelValue('tempMin24h', min(lastTemperature.values()))
+            self.setChannelValue('tempMax24h', max(lastTemperature.values()))    
+        except:
+            self.logger.debug("can not calc temperature data",exc_info=True)        
+
+    def __clearOldTemp(self):
+        timebefor24=int(time())-86400
+        lastTemperature={}
+        lastTemperature=self.getChannelValue('lasttemperature')
+        self.logger.debug("clear last 24h temperature data where older then %s"%(timebefor24))
+        for rainTimeStamp in lastTemperature.copy():
+            if rainTimeStamp<timebefor24:
+                self.logger.debug("time temperature %s < %s befor"%(rainTimeStamp,timebefor24))
+                del lastTemperature[rainTimeStamp]
+        self.setChannelValue('lasttemperature',lastTemperature)    
+                
+                
+                
+                
+                
--- hmc/gateways/cul/devices/ks300_windchill.py
+++ hmc/gateways/cul/devices/ks300_windchill.py
@@ -5,9 +5,72 @@ Created on 24.12.2017
 '''
 from hmc.devices.defaultDevice import device
 
-__version__="2.0"
+__version__="3.0"
 
 
 class device(device):
     def _name_(self):
+        '''
+        give the name of the device back
+        
+        return str name of the device
+        '''
         return "ks300_windchill"
+    
+    def setChannelValue(self,channelName,value):
+        '''
+        set value of channel
+        ''' 
+        channelName=str(channelName)
+        channelName=channelName.lower()
+        try:
+            if not channelName in self._channels:
+                self.logger.error("channel %s is not exist"%(channelName))
+                raise Exception
+            if channelName=='windchill':
+                value=self.__WeatherWindChill(value)
+            self.logger.debug("set channel %s to %s"%(channelName,value))
+            oldValue=self._channels[channelName]['value']['value']
+            self._channels[channelName]['value']['value']=value
+            if oldValue<>value:
+                self._callEvent('onchange_event',channelName)
+            self._callEvent('onrefresh_event',channelName)
+            
+        except:
+            self.logger.error("can not set channel %s to %s"%(channelName,value))
+            raise Exception
+        
+    def __WeatherWindChill(self,wind):
+        '''
+        calculate windchill from value to index
+        ''' 
+        self.logger.debug("calc windchill, wind is:%s"%(wind))  
+        if wind == 0:
+            return "0"
+        elif wind in range(1,6):
+            return 1
+        elif wind in range(6,12):
+            return 2
+        elif wind in range(12,20):
+            return 3
+        elif wind in range(20,29):
+            return 4
+        elif wind in range(29,39):
+            return 5
+        elif wind in range(39,50):
+            return 6
+        elif wind in range(50,62):
+            return 7
+        elif wind in range(62,75):
+            return 8
+        elif wind in range(75,89):
+            return 9
+        elif wind in range(89,103):
+            return 10
+        elif wind in range(103,117):
+            return 11
+        elif wind >116:
+            return 12
+        else:
+            self.logger.error("wind  can not macht from 0 - >116 ")  
+            return "0" 
