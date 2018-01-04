b4c84d81a5f0348170f8c086d733752d6ace5dd0
uschoen <uschoen@johjoh.de>
2018-01-04 18:48:24 +0100

    licent text add

--- hmc/hmc.py
+++ hmc/hmc.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python
-'''
-Created on 23.09.2017
+#
+# Copyright 2017 Ullrich Schoen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# course: cs108
+# laboratory: 1
+#
+#Created on 
+# username: uschoen
+# name: Ullrich Schoen
+#
+# description: a hmc start-up program.
 
-@author: uschoen
-'''
 __version__=2.0
-import sys,getopt,time,json,os,getpass,signal
+
+import sys
+import getopt
+import time
+import json 
+import os
+import getpass
+import signal
 from hmc.core import manager as coreManager
 import socket
 import logging
