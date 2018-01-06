65ed94fdf0010773dfa3cecb2157dcd27e40f378
uschoen <uschoen@johjoh.de>
2018-01-06 21:19:18 +0100

    add new web protocol class

--- hmc/gateways/webAPI/webProtocol.py
+++ hmc/gateways/webAPI/webProtocol.py
@@ -3,6 +3,9 @@ Created on 06.01.2018
 
 @author: uschoen
 '''
+import logging
+import json
+from unittest.test.testmock.testpatch import function
 
 class web(object):
     '''
@@ -11,8 +14,50 @@ class web(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self,params):
         '''
-        Constructor
+        configuration data
         '''
+        self.__params={
+                    "password": "password", 
+                    "user": "user"
+                    }
+        self.__params.update(params)
+        ''' logging instance '''
+        self.__log=logging.getLogger(__name__)
         
+    def decode(self,string):
+        self.__log.info("get new web message to decode")
+        try:
+            
+        except:
+    
+    def encode(self,data,resultStatus): 
+        self.__log.info("get new web message to encode")
+        try:
+            
+        except:
+            
+                
+    def _unserialMSG(self,string):
+        '''
+        unserial a message to a dictionary
+        '''
+        self.__log.debug("unserial message")
+        try:
+            jsonString=json.loads(string)
+            return jsonString
+        except:
+            self.__log.error("can not unserial message %s"%(string),exc_info=True)  
+            
+    def _serialMSG(self,dicVar):
+        '''
+        serial a message to a string
+        '''
+        self.__log.debug("unserial message")
+        try:
+            jsonString=json.dumps(dicVar)
+            return jsonString
+        except:
+            self.__log.error("can not unserial message %s"%(string),exc_info=True)   
+              
