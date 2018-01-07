0f53c3b9d3b0df4995a9c684c04296aa30eaafec
uschoen <uschoen@johjoh.de>
2018-01-07 21:52:48 +0100

    beta API for tinyHome APP

---
+++ hmc/gateways/tinyHomeAPI/APIServer.py
@@ -0,0 +1,213 @@
+'''
+Created on 06.01.2018
+
+@author: uschoen
+'''
+
+
+import threading
+import socket
+import logging
+import time
+import webProtocol
+
+
+__version__="3.0"
+BUFFER=8192
+
+class server(threading.Thread): 
+    def __init__(self, params,core):
+        threading.Thread.__init__(self)
+        ''' core instance '''
+        self.__core=core
+        ''' configuration '''
+        self.__args={
+                    "enable": True, 
+                    "hostName": "unknown", 
+                    "ip": "127.0.0.1", 
+                    "password": "password", 
+                    "port": 9090, 
+                    "timeout": 50, 
+                    "user": "user"
+                    }
+        self.__args.update(params)
+        ''' 
+        allow call able function 
+        TODO: test and add all core function to allowFunktions
+        '''
+        self.__allowFunktions=[
+            "getAllDeviceId",
+            "writeAllConfiguration",
+            "writeEventHandlerFile",
+            "writeDevicesFile"]
+        ''' logger instance '''
+        self.__log=logging.getLogger(__name__) 
+        ''' running flag '''
+        self.running=self.__args.get('enable')
+        ''' blocking time'''
+        self.__blockTime=0
+        ''' server socket '''
+        self.__socket=False
+        ''' recvied msg '''
+        self.__lastMSG=""
+        ''' MSG Start marker 
+        TODO: implement the STARTMARKER in to the protocol'''
+        self.__STARTMARKER="!!!<<<start>>>"
+        ''' MSG End marker '''
+        self.__ENDMARKER="<<<end>>>!!!"
+        ''' webProtocol class '''
+        self.__webProtocol=webProtocol.web({"user":self.__args.get('user'),"password":self.__args.get('password')})
+        
+        self.__log.debug("build  %s instance with version %s"%(__name__,__version__))
+    
+    def run(self):
+        '''
+        run gateway
+        '''
+        try:
+            self.__log.info("%s start"%(__name__))
+            while self.running:
+                self.__buildSocket()
+                if self.__blockTime<int(time.time()):
+                    try:
+                        self.__socket.settimeout(3)
+                        (con, addr) = self.__socket.accept() 
+                        self.__socket.settimeout(None) 
+                        self.__log.debug("get connection from %s"%(addr[0]))
+                        threading.Thread(target=self.__listenToClient,args = (con,addr[0])).start()
+                        self.__log.debug("start new client thread:%s"%(con))
+                    except:
+                        pass
+                time.sleep(0.1)    
+            self.__log.critical("%s normally stop:"%(__name__))
+        except:
+            self.__log.critical("%s have a problem and stop"%(__name__),exc_info=True) 
+          
+    
+    def shutdown(self):
+        '''
+        shutdown server
+        '''
+        self.running=0
+        self.__closeSocket()
+        self.__log.critical("stop %s thread"%(__name__))
+    
+    def __readClientData (self,clientsocket):
+        '''
+        read the data from a socket and check if the endmarker set
+        
+        clientsocket: network socket of the communication
+        
+        exception: raise a exception if a error in the communication
+        '''
+        revData=self.__lastMSG
+        try:
+            while True:
+                data = clientsocket.recv(BUFFER)
+                revData=revData+data
+                if self.__ENDMARKER in revData or not data:
+                    if revData.endswith(self.__ENDMARKER):
+                        self.__lastMSG=""
+                        revData=revData.replace(self.__ENDMARKER,"")
+                        break   
+                    if self.__ENDMARKER in revData:
+                        (revData,self.__lastMSG)=revData.split(self.__ENDMARKER)
+                        break
+                    if not data:
+                        self.__log.error("receive no %s data from core %s %s"%(self.__ENDMARKER,self.__args.get('hostName'),revData))
+                        raise Exception
+            return revData
+        except:
+            self.__log.error("error to receive answer from core %s"%(self.__args.get('hostName')),exc_info=True) 
+            raise Exception
+        
+    def __listenToClient(self,clientsocket,address):
+        '''
+        working client request
+        '''
+        self.__log.debug("get new client request from %s"%(address))
+        while self.running:
+            try:
+                data = self.__readClientData(clientsocket)
+                (function,args,userStatus)=self.__webProtocol.decode(data)
+            except:
+                self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"error in json data"},"0"))
+                break
+            if not userStatus:
+                self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"user or password error"},"0"))
+                break
+            try:
+                if function in self.__allowFunktions:
+                    self.__log.info("try to call function: %s"%(function))
+                    try:
+                        method_to_call = getattr(self.__core,function)
+                        funcArgs=method_to_call(*args)
+                        self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",funcArgs,"1"))
+                        break
+                    except:
+                        self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"function %s have some error"%(function)},"0"))
+                        break
+                else: 
+                    self.__log.error("error function: %s i not allow"%(function))
+                    self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"function_%s not allow"%(function)},"0"))
+                    break
+                     
+            except:
+                self.__log.error("error while fetching data from %s"%(address),exc_info=True)
+                self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"error in request"},"0"))
+                break
+        self.__closeSocket(clientsocket)       
+                
+    
+    def __sendAnwser(self,clientsocket,MSGstring):
+        try:
+            '''TODO: insert start marker'''
+            clientsocket.sendall(MSGstring+self.__ENDMARKER)
+        except:
+            self.__log.error("can not send message",exc_info=True)
+           
+    def __closeSocket(self,clientsocket):
+        ''' close the server socket '''
+        try:
+            if clientsocket:
+                clientsocket.close()
+                self.__log.debug("close socket server")
+        except:
+            self.__log.warning("socket is closed")
+                
+    
+    def __unblockServer(self):
+        '''
+        unblock the server for new connections
+        '''
+        self.__log.info("unblock server ")
+        self.__blockTime=0
+        
+    def __blockServer(self):
+        '''
+        block the server for new connections
+        '''
+        self.__log.error("block server for %i sec"%(self.__args.get('timeout')))
+        self.__blockTime=self.__args.get('timeout')+int(time.time())
+        
+    def __buildSocket(self):
+        '''
+        build a socket connection
+        
+        fetch exception
+        '''
+        if self.__socket or self.__blockTime>int(time.time()):
+            return
+        self.__log.debug("try to build socket ip:%s:%s"%(self.__args.get('ip'),self.__args.get('port')))
+        try:
+            self.__socket=socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+            self.__socket.bind((self.__args.get('ip'),int(self.__args.get('port'))))
+            self.__socket.setblocking(0)
+            self.__socket.listen(5)
+            self.__unblockServer()
+            self.__log.info("socket ip:%s:%s is ready"%(self.__args.get('ip'),self.__args.get('port')))
+        except :
+            self.__blockServer()
+            self.__socket=False
+            self.__log.error("can not build socket ip:%s:%s"%(self.__args.get('ip'),self.__args.get('port')),exc_info=True)
+            
---
+++ hmc/gateways/tinyHomeAPI/webProtocol.py
@@ -0,0 +1,112 @@
+'''
+Created on 06.01.2018
+
+@author: uschoen
+'''
+import logging
+import json
+
+
+class web(object):
+    '''
+    classdocs
+    
+    '''
+
+
+    def __init__(self,params):
+        '''
+        configuration data
+        '''
+        self.__params={
+                    "password": "password", 
+                    "user": "user"
+                    }
+        self.__params.update(params)
+        ''' logging instance '''
+        self.__log=logging.getLogger(__name__)
+        ''' Protocol Version '''
+        self.__VERSION=1
+        
+    def decode(self,string):
+        '''
+        decode a string 
+        
+        return function,args,resutStatus
+        function: function you want to call
+        args: arguments
+        resultStatus=true or false
+        '''
+        self.__log.info("get new web message to decode")
+        try:
+            sendDicVar=self._unserialMSG(string)
+            dicVar={
+                "user":"unknown",
+                "password":"unknown",
+                "version":self.__VERSION,
+                "function":"unknown",
+                "args":{},
+                "resultStatus":0
+                }
+            dicVar.update(sendDicVar)
+            userStatus=False
+            if dicVar.get('user')==self.__params.get('user') and dicVar.get('password')==self.__params.get('user'):
+                userStatus=False
+                self.__log.info("request from user:%s ,check user and password is ok"%(self.__params.get('user')))
+            else:
+                self.__log.warning("request from user:%s ,check user and password is faild"%(self.__params.get('user')))
+            functionName=dicVar.get('function')
+            args=dicVar.get('args')
+            
+            return (functionName,args,userStatus)
+        except:
+            self.__log.error("some error to decode")
+            raise Exception
+    
+    def encode(self,functionName,args,resultStatus):
+        ''' 
+        encode function,args,resultStatus to a string
+        
+        functionName: function you call 
+        args: arguments to give back
+        resultStatus: true or false, true=success false=fail
+        
+        return a string
+        give the exception back
+        '''
+        self.__log.info("get new message to encode")
+        try:
+            dicVar={
+                "user":self.__params.get('user'),
+                "password":self.__params.get('user'),
+                "version":self.__VERSION,
+                "function":functionName,
+                "args":args,
+                "resultStatus":resultStatus
+                }
+            MSGstring=self._serialMSG(dicVar)
+            return MSGstring
+        except:
+            self.__log.error("can not encode the message",exc_info=True)
+                
+    def _unserialMSG(self,string):
+        '''
+        unserial a message to a dictionary
+        '''
+        self.__log.debug("unserial message")
+        try:
+            jsonString=json.loads(string)
+            return jsonString
+        except:
+            self.__log.error("can not unserial message %s to a dictionary"%(string),exc_info=True)  
+            
+    def _serialMSG(self,dicVar):
+        '''
+        serial a message to a string
+        '''
+        self.__log.debug("serial message")
+        try:
+            jsonString=json.dumps(dicVar)
+            return jsonString
+        except:
+            self.__log.error("can not serial message to a string",exc_info=True)
