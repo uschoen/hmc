59a20e68faad8ffea61efa715390c04498322e24
uschoen <uschoen@johjoh.de>
2018-01-07 21:35:30 +0100

    final Version off Web API

--- hmc/gateways/webAPI/APIServer.py
+++ hmc/gateways/webAPI/APIServer.py
@@ -10,7 +10,7 @@ import socket
 import logging
 import time
 import webProtocol
-from gateways.webAPI import webProtocol
+
 
 __version__="3.0"
 BUFFER=8192
@@ -22,7 +22,7 @@ class server(threading.Thread):
         self.__core=core
         ''' configuration '''
         self.__args={
-                    "enable": False, 
+                    "enable": True, 
                     "hostName": "unknown", 
                     "ip": "127.0.0.1", 
                     "password": "password", 
@@ -31,6 +31,15 @@ class server(threading.Thread):
                     "user": "user"
                     }
         self.__args.update(params)
+        ''' 
+        allow call able function 
+        TODO: test and add all core function to allowFunktions
+        '''
+        self.__allowFunktions=[
+            "getAllDeviceId",
+            "writeAllConfiguration",
+            "writeEventHandlerFile",
+            "writeDevicesFile"]
         ''' logger instance '''
         self.__log=logging.getLogger(__name__) 
         ''' running flag '''
@@ -41,28 +50,39 @@ class server(threading.Thread):
         self.__socket=False
         ''' recvied msg '''
         self.__lastMSG=""
-        ''' MSG Start marker '''
+        ''' MSG Start marker 
+        TODO: implement the STARTMARKER in to the protocol'''
         self.__STARTMARKER="!!!<<<start>>>"
         ''' MSG End marker '''
         self.__ENDMARKER="<<<end>>>!!!"
         ''' webProtocol class '''
-        self.__webProtocol=webProtocol.web()
+        self.__webProtocol=webProtocol.web({"user":self.__args.get('user'),"password":self.__args.get('password')})
+        
         self.__log.debug("build  %s instance with version %s"%(__name__,__version__))
     
     def run(self):
         '''
         run gateway
         '''
-        self.__log.info("%s start"%(__name__))
-        while self.running:
-            self.__buildSocket()
-            if self.__blockTime<int(time.time()):
-                (con, addr) = self.__socket.accept()  
-                self.__log.debug("get connection from %s"%(addr[0]))
-                threading.Thread(target=self.__listenToClient,args = (con,addr)).start()
-                self.__log.debug("start new client thread:%s"%(con))
-            time.sleep(0.1)    
-        self.__log.critical("%s stop:"%(__name__))   
+        try:
+            self.__log.info("%s start"%(__name__))
+            while self.running:
+                self.__buildSocket()
+                if self.__blockTime<int(time.time()):
+                    try:
+                        self.__socket.settimeout(3)
+                        (con, addr) = self.__socket.accept() 
+                        self.__socket.settimeout(None) 
+                        self.__log.debug("get connection from %s"%(addr[0]))
+                        threading.Thread(target=self.__listenToClient,args = (con,addr[0])).start()
+                        self.__log.debug("start new client thread:%s"%(con))
+                    except:
+                        pass
+                time.sleep(0.1)    
+            self.__log.critical("%s normally stop:"%(__name__))
+        except:
+            self.__log.critical("%s have a problem and stop"%(__name__),exc_info=True) 
+          
     
     def shutdown(self):
         '''
@@ -95,11 +115,11 @@ class server(threading.Thread):
                         break
                     if not data:
                         self.__log.error("receive no %s data from core %s %s"%(self.__ENDMARKER,self.__args.get('hostName'),revData))
-                        raise
+                        raise Exception
             return revData
         except:
-            self.__log.error("error to receive answer from core %s"%(self.__args.get('hostName'),exc_info=True) 
-            raise  
+            self.__log.error("error to receive answer from core %s"%(self.__args.get('hostName')),exc_info=True) 
+            raise Exception
         
     def __listenToClient(self,clientsocket,address):
         '''
@@ -109,17 +129,46 @@ class server(threading.Thread):
         while self.running:
             try:
                 data = self.__readClientData(clientsocket)
-                (function,args)=self.__webProtocol.decode(data) #TODO: define args to take over to webProtokoll
-                ''' TODO: write function to end
+                (function,args,resultStatus)=self.__webProtocol.decode(data)
+            except:
+                self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"error in json data"},"0"))
+                break
+            try:
+                if function in self.__allowFunktions:
+                    self.__log.info("try to call function: %s"%(function))
+                    try:
+                        method_to_call = getattr(self.__core,function)
+                        funcArgs=method_to_call(*args)
+                        self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",funcArgs,"1"))
+                        break
+                    except:
+                        self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"function %s have some error"%(function)},"0"))
+                        break
+                else: 
+                    self.__log.error("error function: %s i not allow"%(function))
+                    self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"function_%s not allow"%(function)},"0"))
+                    break
+                     
             except:
                 self.__log.error("error while fetching data from %s"%(address),exc_info=True)
+                self.__sendAnwser(clientsocket,self.__webProtocol.encode("result",{"message":"error in request"},"0"))
+                break
+        self.__closeSocket(clientsocket)       
+                
     
-    def __closeSocket(self):
+    def __sendAnwser(self,clientsocket,MSGstring):
+        try:
+            '''TODO: insert start marker'''
+            clientsocket.sendall(MSGstring+self.__ENDMARKER)
+        except:
+            self.__log.error("can not send message",exc_info=True)
+           
+    def __closeSocket(self,clientsocket):
         ''' close the server socket '''
         try:
-            if self.__socket:
-                self.__socket.close()
-                self.__log.critical("close socket server")
+            if clientsocket:
+                clientsocket.close()
+                self.__log.debug("close socket server")
         except:
             self.__log.warning("socket is closed")
                 
@@ -146,15 +195,16 @@ class server(threading.Thread):
         '''
         if self.__socket or self.__blockTime>int(time.time()):
             return
-        self.__log.debug("try to build socket ip:%s:%s"%(self.args.get('ip'),self.args.get('port')))
+        self.__log.debug("try to build socket ip:%s:%s"%(self.__args.get('ip'),self.__args.get('port')))
         try:
             self.__socket=socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-            self.__socket.bind((self.args.get('ip'),int(self.args.get('port'))))
+            self.__socket.bind((self.__args.get('ip'),int(self.__args.get('port'))))
+            self.__socket.setblocking(0)
             self.__socket.listen(5)
-            self.____unblockServer()
-            self.__log.info("socket ip:%s:%s is ready"%(self.args.get('ip'),self.args.get('port')))
+            self.__unblockServer()
+            self.__log.info("socket ip:%s:%s is ready"%(self.__args.get('ip'),self.__args.get('port')))
         except :
             self.__blockServer()
             self.__socket=False
-            self.__log.error("can not build socket ip:%s:%s"%(self.args.get('ip'),self.args.get('port')),exc_info=True)
+            self.__log.error("can not build socket ip:%s:%s"%(self.__args.get('ip'),self.__args.get('port')),exc_info=True)
             
--- hmc/gateways/webAPI/webProtocol.py
+++ hmc/gateways/webAPI/webProtocol.py
@@ -5,7 +5,7 @@ Created on 06.01.2018
 '''
 import logging
 import json
-from unittest.test.testmock.testpatch import function
+
 
 class web(object):
     '''
@@ -25,19 +25,66 @@ class web(object):
         self.__params.update(params)
         ''' logging instance '''
         self.__log=logging.getLogger(__name__)
+        ''' Protocol Version '''
+        self.__VERSION=1
         
     def decode(self,string):
+        '''
+        decode a string 
+        
+        return function,args,resutStatus
+        function: function you want to call
+        args: arguments
+        resultStatus=true or false
+        '''
         self.__log.info("get new web message to decode")
         try:
-            
+            sendDicVar=self._unserialMSG(string)
+            dicVar={
+                "user":"unknown",
+                "password":"unknown",
+                "version":self.__VERSION,
+                "function":"unknown",
+                "args":{},
+                "resultStatus":0
+                }
+            dicVar.update(sendDicVar)
+            '''
+            TODO: User and password check
+            '''
+            functionName=dicVar.get('function')
+            args=dicVar.get('args')
+            resultStatus=dicVar.get('resultStatus')
+            return (functionName,args,resultStatus)
         except:
+            self.__log.error("some error to decode")
+            raise Exception
     
-    def encode(self,data,resultStatus): 
-        self.__log.info("get new web message to encode")
+    def encode(self,functionName,args,resultStatus):
+        ''' 
+        encode function,args,resultStatus to a string
+        
+        functionName: function you call 
+        args: arguments to give back
+        resultStatus: true or false, true=success false=fail
+        
+        return a string
+        give the exception back
+        '''
+        self.__log.info("get new message to encode")
         try:
-            
+            dicVar={
+                "user":self.__params.get('user'),
+                "password":self.__params.get('user'),
+                "version":self.__VERSION,
+                "function":functionName,
+                "args":args,
+                "resultStatus":resultStatus
+                }
+            MSGstring=self._serialMSG(dicVar)
+            return MSGstring
         except:
-            
+            self.__log.error("can not encode the message",exc_info=True)
                 
     def _unserialMSG(self,string):
         '''
@@ -48,16 +95,15 @@ class web(object):
             jsonString=json.loads(string)
             return jsonString
         except:
-            self.__log.error("can not unserial message %s"%(string),exc_info=True)  
+            self.__log.error("can not unserial message %s to a dictionary"%(string),exc_info=True)  
             
     def _serialMSG(self,dicVar):
         '''
         serial a message to a string
         '''
-        self.__log.debug("unserial message")
+        self.__log.debug("serial message")
         try:
             jsonString=json.dumps(dicVar)
             return jsonString
         except:
-            self.__log.error("can not unserial message %s"%(string),exc_info=True)   
-              
+            self.__log.error("can not serial message to a string",exc_info=True)
