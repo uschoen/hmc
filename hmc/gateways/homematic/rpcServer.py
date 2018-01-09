1207e61839a1179ad20efb175f2dc1202f23f3ec
uschoen <uschoen@johjoh.de>
2018-01-09 13:39:54 +0100

    rebuilt homematic rpc server

--- hmc/gateways/homematic/rpcServer.py
+++ hmc/gateways/homematic/rpcServer.py
@@ -3,85 +3,103 @@ Created on 22.10.2016
 
 @author: uschoen
 install :
-apt-get install python-xmltodict
+sudo apt-get install python-xmltodict
 
-TODO:
-rebuild script and class
+
 '''
 
-__version__= "3.0"
+__version__= "3.1"
 
 import threading
+import logging
+import time
+from random import randint
 import xmlrpclib                                            #@UnresolvedImport
 from SimpleXMLRPCServer import SimpleXMLRPCServer           #@UnresolvedImport
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler   #@UnresolvedImport @UnusedImport
-import urllib2                                              #@UnresolvedImport
-import xmltodict                                            #@UnresolvedImport
-from random import randint
-import logging
 
 
-
-from time import sleep, time
-
-
-
-
-class hmc_rpc_callback:
-    def __init__(self,parms,core,timer):
+class hmcRPCcallback:
+    '''
+    call back function from the RPC Server
+    to handle request
+    '''
+    def __init__(self,parms,core,resetTimer):
+        ''' core instance '''
         self.__core=core
+        ''' configuration parameter  '''
+        self.__config={
+                        "gateway":"unknown",
+                        "host":"unknown"
+                      }
         self.__config=parms
-        self.__timer=timer
-        self.logger=logging.getLogger(__name__) 
+        ''' reset timer function '''
+        self.__resetTimer=resetTimer
+        ''' logging instance '''
+        self.__log=logging.getLogger(__name__) 
     
-    def event(self, interfaceID, device, channelName, value,*unkown):
-        (device,channelNumber)=device.split(":")
-        deviceID=("%s@%s.%s"%(device,self.__config['gateway'],self.__config['host']))  
-        channelName=channelName.lower()
-        self.__timer()
-        try:
-            self.logger.debug("event: interfaceID = %s, device: %s, channel name = %s, value = %s" % ( interfaceID,device,channelName,value))
-            self.logger.debug("deviceID %s"%(deviceID))
+    def event(self,interfaceID,deviceNumber,channelName,value,*unkown):
+        '''
+        only for logging
+        '''
+        self.__resetTimer()
+        file = open('log/channels.txt','a') 
+        file.write('%s %s %s %s\n'%(deviceNumber,channelName,value,unkown))
+        file.close() 
         
+        deviceID=("%s@%s.%s"%(deviceNumber,self.__config.get('gateway'),self.__config.get('host')))  
+        channelName=channelName.lower()
+        
+        try:
+            self.__log.debug("event: deviceID: %s, channel name = %s, value = %s" % (deviceID,channelName,value))
             if not self.__core.ifDeviceExists(deviceID):
-                self.logger.error("deviceID %s is not exists"%(deviceID))
+                self.__log.error("deviceID %s is not exists"%(deviceID))
                 return''
             if not self.__core.ifDeviceChannelExist(deviceID,channelName):
                 self.__core.addDeviceChannel(deviceID,channelName,self.__defaultAttribute(channelName))
         except:
-            self.logger.error("error to add channel %s for deviceID %s "%(channelName,deviceID), exc_info=True)
+            self.__log.error("error to add channel %s for deviceID %s "%(channelName,deviceID), exc_info=True)
             return ''
         try:
             self.__core.setDeviceChannelValue(deviceID,channelName,value)
         except:
-            self.logger.error("error can not  add value %s for channel %s for deviceID %s "%(value,channelName,deviceID), exc_info=True)
-        return ''
-    def listMethods(self,*args):
-        self.__timer()
-        self.logger.info("call listMethods for interfaceID %s"%(args))
+            self.__log.error("error can not  add value %s for channel %s for deviceID %s "%(value,channelName,deviceID), exc_info=True)
         return ''
     
-    def listDevices(self,*args):
-        self.__timer()
-        self.logger.info("call listDevices for interfaceID %s"%(args))
-        return ''
-    
-    def newDevices(self, interfaceID, allDevices):
-        self.__timer()
-        self.logger.debug("call newDevices for interfaceID:%s"%(interfaceID))
+    def newDevices(self, interfaceID,allDevices):
+        self.__resetTimer()
+        self.__log.debug("call newDevices for interfaceID:%s"%(interfaceID))
         for device in allDevices:
-            if device['PARENT']=="":
-                deviceID="%s@%s.%s"%(device['ADDRESS'],self.__config['gateway'],self.__config['host'])
+            '''
+            only for debug
+            '''
+            file = open('log/devices.txt','a') 
+            file.write('%s\n'%(device))
+            file.close() 
+            if not device['PARENT']=="":
+                deviceID="%s@%s.%s"%(device['ADDRESS'],self.__config.get('gateway'),self.__config.get('host'))
                 try:
                     if not self.__core.ifDeviceExists(deviceID):
                         self.__addNewDevice(device)
                     else:
-                        self.logger.info("deviceID is exist: %s"%(deviceID))
+                        self.__log.info("deviceID is exist: %s"%(deviceID))
                 except:
-                    self.logger.warning("can not add deviceID: %s"%(deviceID), exc_info=True) 
+                    self.__log.warning("can not add deviceID: %s"%(deviceID), exc_info=True) 
         return ''
-     
+    def listMethods(self,*args):
+        self.__resetTimer()
+        self.__log.info("call listMethods for interfaceID %s"%(args))
+        return ''
+    
+    def listDevices(self,*args):
+        self.__resetTimer()
+        self.__log.info("call listDevices for interfaceID %s"%(args))
+        return ''
+    
     def __defaultAttribute(self,channelName):
+        '''
+        return a dic with default Attribute
+        '''
         channelValues={}
         channelValues[channelName]={
                 "value":{
@@ -99,8 +117,8 @@ class hmc_rpc_callback:
         '''
         try:
             deviceID="%s@%s.%s"%(device['ADDRESS'],self.__config['gateway'],self.__config['host'])
-            self.logger.info("add  new devicesID:%s"%(deviceID))
-            devicetype=device['TYPE'].replace("-","_")
+            self.__log.info("add  new devicesID:%s"%(deviceID))
+            devicetype=device['PARENT_TYPE'].replace("-","_")
             device={
                     "gateway":{
                         "value":"%s"%(self.__config['gateway']),
@@ -128,154 +146,179 @@ class hmc_rpc_callback:
             channel={}
             self.__core.addDevice(device,channel)
         except:
-            self.logger.error("can not add deviceID: %s"%(deviceID), exc_info=True) 
+            self.__log.error("can not add deviceID: %s"%(deviceID), exc_info=True) 
                             
     
             
-        
+
+
 
 class server(threading.Thread):
-    '''
-    classdocs
-    '''
+    
     def __init__(self,params,core):
-        '''
-        Constructor
-        '''
         threading.Thread.__init__(self)
-        self.running=1
-        self.__core=core
-        self.__timer=int(time())
-        self.__timeoutnow=False
+        '''         configuration        '''
         self.__config={
-                    "autoAttribut":True,
                     "name":"hmc_rpc_rf_default",
                     "hm_ip":"127.0.0.1",
                     "hm_port":"2000",
                     "hm_interface_id":randint(100,999),
-                    "rpc_port":5050+randint(0,100),
+                    "rpc_port":5050+randint(0,49),
                     "rpc_ip":"127.0.0.0",
-                    "gateway":"unkown",
-                    "host":"unkown",
-                    "timeout":280}
+                    "gateway":"unknown",
+                    "host":"unknown",
+                    "blockRPCServer":60,
+                    "MSGtimeout":60}
         self.__config.update(params)
-        self.logger=logging.getLogger(__name__) 
-        self.logger.debug("build  %s instance"%(__name__))
+        '''    logger instance    '''
+        self.__log=logging.getLogger(__name__) 
+        '''    running flag    '''
+        self.running=1
+        '''    core instance   '''
+        self.__core=core
+        '''    timeout         '''
+        self.__lastMSG=0
+        '''    timeout for server '''
+        self.__blockTime=0
+        '''    rpc Server Instance '''
+        self.__rpcServer=False
+        
+        self.__log.debug("build  %s instance"%(__name__))
     
     def run(self):
-        self.logger.info( "start thread")
-        while not self.__adding_server():
-            self.logger.error("can not start RPC server wait 120 sec. and try again")
-            sleep(120)
-        self.logger.info("wait 2 sec to INIT RPC Connect")
-        sleep(2)
-        self.__RPC_connect()
-        self.__RPC_Init()
-        while self.running:
-            if (self.__timer+self.__config['timeout'] < int(time())):
-                self.logger.error("timeout, no messages from homematic")
-                if not self.__timeoutnow:
-                    self.__sendcommand()
-                    self.__timer=int(time())
-                    self.__timeoutnow=True
-                else:
-                    self.__RPC_stop()
-                    sleep(1)
-                    self.__RPC_Init()
-                    self.resetTimer()
-            sleep(0.1) 
-        self.logger.warning("thread %s stop"%(__name__))
-       
-    def __sendcommand(self):
-        self.logger.error("send messages to homematic")
-        HMurl=self.__config['hmXmlUrl']
-        iseID=self.__config['iseID']
-        value=self.__config['iseIDValue']
-        url=("%s?ise_id=%s&new_value=%s"%(HMurl,iseID,value))
-        self.logger.debug("url is %s "%(url))
         try:
-            response = urllib2.urlopen(url)
-            httcode=response.getcode()
-            self.logger.debug("http response code %s"%(httcode))
-            if  httcode<>200:
-                self.logger.error("can not send request to url") 
-                return
-            xml=response.read()
-            self.logger.debug("xml response %s"%(xml))
-            HMresponse=xmltodict.parse(xml)
-            if "result" in HMresponse:
-                if "changed" in HMresponse['result']: 
-                    self.logger.debug("value successful change")
-                elif "not_found" in HMresponse['result']: 
-                    self.logger.error("can not found iseID %s"%(iseID))
-                else:
-                    self.logger.warning("get some unkown answer %s"%(xml))
-            else:
-                self.logger.warning("get some unkown answer %s"%(xml)) 
-        except urllib2.URLError:
-            self.logger.error("get some error at url request", exc_info=True)
-        except ValueError:
-            self.logger.error("some error at request, check configuration", exc_info=True)
-        except :
-            self.logger.critical("somthing going wrong !!!", exc_info=True)    
+            self.__log.info("%s start"%(__name__))
+            while self.running:
+                self.__buildRPCServer()
+                if self.__blockTime<int(time.time()):
+                    if self.__lastMSG<int(time.time()):
+                        self.__log.warning("message timeout, get no message since %i sec."%(self.__config.get('MSGtimeout')))
+                        try:
+                            self.__startInitRequest()
+                        except:
+                            self.__log.warning("can not request a RPC INIT")
+                            self.__blockServer() 
+                time.sleep(0.1)    
+            self.__log.critical("%s normally stop:"%(__name__))
+        except:
+            self.__log.critical("%s have a problem and stop"%(__name__),exc_info=True)
+            self.__stopInitRequest()
         
-    def __adding_server(self):
+    def shutdown(self):
+        '''
+        shutdown
+        '''
+        self.running=False
+        self.__log.critical("RPC Server shutdown")
         try:
-            self.logger.info("rpc Server %s:%s start"%(self.__config["rpc_ip"],self.__config["rpc_port"]))
-            server = SimpleXMLRPCServer((self.__config["rpc_ip"],int(self.__config["rpc_port"])))
+            self.__rpcServer=False
+            self.__stopInitRequest()
+        except:
+            self.__log.critical("error stopping RPC connection")
+        self.__log.critical("RPC Server is stop")
+    
+    def __startInitRequest(self):
+        '''
+        send a init Request to get data
+        
+        raise exception on failed
+        '''
+        try:
+            proxy=self.__proxyInstance()
+            self.__log.info("send a RPC INIT Request at:(http://%s:%s)"%(self.__config.get("rpc_ip"),self.__config.get("rpc_port")))
+            proxy.init("http://%s:%s"%(self.__config.get("rpc_ip"), self.__config.get("rpc_port")),self.__config.get("hm_interface_id"))
+            self.__log.debug("send a RPC INIT Request finish")
+        except:
+            self.__log.error("can not start send a start INIT request",exc_info=True)
+            raise Exception
+   
+    def __stopInitRequest(self):
+        '''
+        send a stop Request to get no data
+        
+        raise exception on failed
+        '''
+        try:
+            proxy=self.__proxyInstance()
+            self.__log.info("stop RPC Request at:http://%s:%s"%(self.__config.get("rpc_ip"),self.__config.get("rpc_port")))
+            proxy.init("http://%s:%s"%(self.__config["rpc_ip"], int(self.__config["rpc_port"])))
+            self.__log.debug("stopped RPC data from homematic")
+        except:
+            self.__log.error("can not send a stop INIT request",exc_info=True)
+            raise Exception
+            
+    def __proxyInstance(self):
+        '''
+        build a RPC proxy instance
+        '''
+        try:
+            self.__log.info("RPC Init: init(http://%s:%s, '%s')" %(self.__config.get("rpc_ip"),int(self.__config.get("rpc_port")), self.__config.get("hm_interface_id")) )
+            proxy = xmlrpclib.ServerProxy("http://%s:%i" %(self.__config.get("hm_ip"), int(self.__config.get("hm_port"))))
+            self.__log.debug("proxy initialized")
+            return proxy
+        except:
+            self.__log.error("can not build a RPC proxy server",exc_info=True)
+            raise Exception
+            
+    def __buildRPCServer(self):
+        '''
+        build a RPC Server
+        '''
+        if self.__blockTime>int(time.time()):
+            ''' server is block '''
+            return
+        try:
+            if self.__rpcServer:
+                if self.__rpcServer.isAlive():
+                    ''' server is running '''
+                    return
+                else:
+                    ''' server is exciting but not running '''
+                    self.__log.warning("RPC Server %s:%s is stop,starting server"%(self.__config.get("rpc_ip"),self.__config.get("rpc_port")))
+                    try:
+                        self.__rpcServer.start()
+                        self.__unblockServer() 
+                        return
+                    except:
+                        self.__blockServer()
+                        self.__rpcServer=False
+                        self.__log.warning("can not start RPC Server %s:%s"%(self.__config.get("rpc_ip"),self.__config.get("rpc_port")))
+                        return
+            self.__log.info("RPC Server %s:%s start"%(self.__config.get("rpc_ip"),self.__config.get("rpc_port")))
+            server = SimpleXMLRPCServer((self.__config.get("rpc_ip"),int(self.__config.get("rpc_port"))))
             server.logRequests=False
             server.register_introspection_functions() 
             server.register_multicall_functions() 
-            server.register_instance(hmc_rpc_callback(self.__config,self.__core,self.resetTimer))
-            self.__rpc_server = threading.Thread(target=server.serve_forever)
-            self.__rpc_server.start()
-            self.logger.info("rpc Server Start")
-            return True
+            server.register_instance(hmcRPCcallback(self.__config,self.__core,self.__resetTimer))
+            self.__rpcServer = threading.Thread(target=server.serve_forever)
+            self.__rpcServer.start()
+            self.__log.info("RPC Server Start")
+            self.__unblockServer()
         except:
-            self.logger.error("can not start rpc server", exc_info=True)
-            server=False
-            return False
-    '''
-    shutdown
-    '''
-    def shutdown(self):
-        self.running=False
-        self.logger.critical("rpc Server stop")
-        try:
-            self.__RPC_connect()
-            self.__RPC_stop()
-        except:
-            self.logger.critical("error stopping rpc connection")
-        self.logger.critical("rpc Server is stop")
-    '''
-    Logging Intance
-    ''' 
-    def __RPC_Init(self):
-        self.logger.info("RPC Init: init(http://%s:%i, '%s')" % (self.__config["rpc_ip"],int(self.__config["rpc_port"]), self.__config["hm_interface_id"]) )
-        try:
-            self.proxy.init("http://%s:%i" % (self.__config["rpc_ip"], int(self.__config["rpc_port"])), self.__config["hm_interface_id"])
-            self.logger.debug("Proxy initialized")
-        except:
-            self.logger.error("Failed to initialize proxy", exc_info=True)
-            raise Exception
-    def __RPC_connect(self):
-        self.logger.debug("Creating proxy. Connecting to http://%s:%i" % (self.__config["hm_ip"], int(self.__config["hm_port"])))
-        try:
-            self.proxy = xmlrpclib.ServerProxy("http://%s:%i" % (self.__config["hm_ip"], int(self.__config["hm_port"])))
-            self.logger.debug("start rpc data from homematic")
-        except:
-            self.logger.warning("Failed connecting to proxy at http://%s:%i" % (self.__config["hm_ip"], int(self.__config["hm_port"])), exc_info=True)
-            raise Exception
-    def __RPC_stop(self):
-        self.logger.debug("stopping RPC data from Homematic. Connecting to http://%s:%i" % (self.__config["hm_ip"], int(self.__config["hm_port"])))
-        self.logger.info("RPC Init STOP: init(http://%s:%i)" % (self.__config["rpc_ip"],int(self.__config["rpc_port"])) )
-        try:
-            self.proxy.init("http://%s:%i" % (self.__config["rpc_ip"], int(self.__config["rpc_port"])))
-            self.logger.debug("stopped rpc data from homematic")
-        except:
-            self.logger.error("Failed to stop RPC ", exc_info=True)
-            raise Exception
-    def resetTimer(self):
-        self.logger.debug("reset timeout timer")
-        self.__timer=int(time())
-        self.__timeoutnow=False
+            self.__log.error("can not start RPC server", exc_info=True)
+            self.__rpcServer=False
+            self.__blockServer() 
+    
+    def __resetTimer(self):
+        ''' reset timer '''
+        self.__log.debug("reset MSG timeout")
+        self.__lastMSG=self.__config.get('MSGtimeout')+int(time.time())  
+        
+    def __unblockServer(self):
+        '''
+        unblock the server for new connections
+        '''
+        self.__log.info("unblock server in 2 sec.")
+        self.__blockTime=2+int(time.time()) 
+        
+    def __blockServer(self):
+        '''
+        block the server for new connections
+        '''
+        self.__log.error("block server for %i sec"%(self.__config.get('blockRPCServer')))
+        self.__blockTime=self.__config.get('blockRPCServer')+int(time.time())    
+        
+        
+        
+       
+           
