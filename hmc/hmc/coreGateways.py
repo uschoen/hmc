c59e52fe0d6e271e36dbf7ac8d77a1546b7ffab9
uschoen <uschoen@johjoh.de>
2018-01-15 23:24:11 +0100

    add new function, addGateway updateGateway deleteGateway restoreGateway __buildGateway

--- hmc/hmc/coreGateways.py
+++ hmc/hmc/coreGateways.py
@@ -3,10 +3,11 @@ Created on 23.09.2017
 
 @author: uschoen
 '''
-__version__=0.2
+__version__=3.1
 
 from gateways import *
 import importlib
+
 
 class coreGateways():
     '''
@@ -40,50 +41,199 @@ class coreGateways():
         else:
             self.logger.error("gateway instance %s not found"%(gatewayName))
             return False
-           
+    
+    def restoreGateway(self,gatewayName,config):
+        ''' 
+        restore a Gateway, the gateway name have to be unique and not exisitng,
+        after restore its not update the other core server. This ist only
+        a action for startup !!! 
+        
+        gatewayName: name of the gateway 
+        config: configuration of the gateways
+            {
+            "enable": enable the gateway, if the gateway not enable it will not build only store in cfg file
+            "package":package name of the gateways
+            "modul": module name of the gateway
+            "class": class of the module
+            "config":configuration of the gateways class
+            "host": host name on wich is running the gateway
+            }
+        
+        raise exception on failure
+        '''
+        self.logger.info("restore gateway %s"%(gatewayName))
+        try:
+            self.__buildGateway(gatewayName,config)
+            self.startGateway(gatewayName)  
+        except:
+            self.logger.info("can not add gateway %s"%(gatewayName))
+            raise Exception
+        
     def addGateway(self,gatewayName,config):
-        self.logger.info("add gateway %s"%(gatewayName))
-        self.gatewaysCFG[gatewayName]=config 
-        if self.eventHome(gatewayName): 
-            pakage="gateways.%s.%s"%(config['package'],config['modul'])
-            self.logger.info("try to load gateway: %s  with package: %s"%(gatewayName,pakage))
-            tempconfig=config['config']
-            tempconfig['package']=config['package']
-            tempconfig.update(self.args['global'])
-            ARGUMENTS = (tempconfig,self)  
+        ''' 
+        add a Gateway, the gateway name have to be unique an not exsisting
+        after add its update the other core server
+        
+        gatewayName: name of the gateway 
+        config: configuration of the gateways
+            {
+            "enable": enable the gateway, if the gateway not enable it will not build only store in cfg file
+            "package":package name of the gateways
+            "modul": module name of the gateway
+            "class": class of the module
+            "config":configuration of the gateways class
+            "host": host name on wich is running the gateway
+            }
+        
+        raise exception on failure
+        '''
+        self.logger.info("restore gateway %s"%(gatewayName))
+        try:
+            self.__buildGateway(gatewayName,config)  
+            self.startGateway(gatewayName)      
+            self.updateRemoteCore(False,gatewayName,'addGateway',gatewayName,config)
+        except:
+            self.logger.info("can not add gateway %s"%(gatewayName))
+            raise Exception
+        
+     
+    def updateGateway(self,gatewayName,config):
+        ''' 
+        update a Gateway, the gateway name have to be unique can be exsisting.
+        If a Gateway exsiting it will be stop and delete.
+        after update its update the other core server
+        
+        gatewayName: name of the gateway 
+        config: configuration of the gateways
+            {
+            "enable": enable the gateway, if the gateway not enable it will not build only store in cfg file
+            "package":package name of the gateways
+            "modul": module name of the gateway
+            "class": class of the module
+            "config":configuration of the gateways class
+            "host": host name on wich is running the gateway
+            }
+        
+        raise exception on failure
+        '''
+        self.logger.info("restore gateway %s"%(gatewayName))
+        try:
+            if config==self.gatewaysCFG.get(gatewayName,{}):
+                self.logger.info("no update need, %s gateway have same setting"%(gatewayName))
+                return
+            self.deleteGateway(gatewayName)
+            self.__buildGateway(gatewayName,config)
+            self.startGateway(gatewayName)
+            self.updateRemoteCore(False,gatewayName,'updateGateway',gatewayName,config)
+        except:
+            self.logger.debug("can not restore gateway %s"%(gatewayName))
+            raise Exception
+    
+    
+    def deleteGateway(self,gatewayName):
+        ''' 
+        delete a gateway
+        
+        gatewaysName: the name of the gateway
+        
+        raise all exceptions
+        '''
+        self.logger.debug("delete gateway %s"%(gatewayName))
+        if gatewayName not in self.gatewaysInstance:
+            self.logger.error("gateway %s is not existing"%(gatewayName))
+            raise Exception
+        if self.gatewaysInstance['status']<>"stop":
             try:
-                module = importlib.import_module(pakage)
-                CLASS_NAME = config['class']
-                self.gatewaysInstance[gatewayName]={}
-                self.gatewaysInstance[gatewayName]['name']=gatewayName
-                self.gatewaysInstance[gatewayName]['status']="stop"
-                self.gatewaysInstance[gatewayName]['instance'] = getattr(module, CLASS_NAME)(*ARGUMENTS)
-                self.gatewaysInstance[gatewayName]['instance'].daemon = True
-                self.gatewaysInstance[gatewayName]['enable']=config['enable']
-                if config['enable']:
-                    self.logger.info("start gateway %s"%(gatewayName))
-                    self.startGateway(gatewayName)
+                self.stopGateway(self.gatewaysInstance['instance'])
+            except:
+                pass
+        del self.gatewaysInstance[gatewayName]
+        if  gatewayName in self.gatewaysCFG:  
+            del self.gatewaysCFG[gatewayName]     
+                  
+    def __buildGateway(self,gatewayName,config):
+        '''
+        build a gateway
+        
+        gatewayName: name of the gateway 
+        config: configuration of the gateways
+            {
+            "enable": enable the gateway, if the gateway not enable it will not build only store in cfg file
+            "package":package name of the gateways
+            "modul": module name of the gateway
+            "class": class of the module
+            "config":configuration of the gateways class
+            "host": host name on wich is running the gateway
+            }
+            
+        raise exception on failure
+        '''
+        if gatewayName in  self.gatewaysInstance or gatewayName in self.gatewaysCFG:
+            self.logger.error("gateway %s exists as Instance or configuration "%(gatewayName))
+            raise Exception
+        self.logger.info("build gateway %s"%(gatewayName))
+        ''' default configuration with update from gateways cfg (config) '''
+        defaultGatewayCFG={
+                    "enable":False,
+                    "package":"unknown",
+                    "modul":"unknown",
+                    "class":"unkown",
+                    "config":{},
+                    "host":"unknown"
+                    }
+        defaultGatewayCFG.update(config)
+        defaultGatewayCFG.update(self.args.get('global'))
+        if not defaultGatewayCFG['enable']:
+            defaultGatewayCFG['config']['enable']=False
+        self.gatewaysCFG[gatewayName]=defaultGatewayCFG
+        '''
+        try to build the gateway instance '''         
+        if self.eventHome(gatewayName): 
+            try:
+                if defaultGatewayCFG.get('enable'):
+                    pakage="gateways.%s.%s"%(defaultGatewayCFG.get('package'),defaultGatewayCFG.get('modul'))
+                    CLASS_NAME = defaultGatewayCFG.get('class')
+                    ARGUMENTS = (defaultGatewayCFG.get('config'),self)
+                    
+                    self.logger.info("try to load gateway: %s  with package: %s"%(gatewayName,pakage))
+                    self.gatewaysInstance[gatewayName]={
+                                                'name':gatewayName,
+                                                'status':"stop",
+                                                'enable':False,
+                                                'instance':False
+                                                }
+                    module = importlib.import_module(pakage)
+                    self.gatewaysInstance[gatewayName]['instance'] = getattr(module, CLASS_NAME)(*ARGUMENTS)
+                    self.gatewaysInstance[gatewayName]['instance'].daemon = True
+                    self.gatewaysInstance[gatewayName]['enable']=True
                 else:
                     self.logger.info("gateway %s is disable"%(gatewayName)) 
-                self.updateRemoteCore(False,gatewayName,'addGateway',gatewayName,config)
+                    
             except :
                 self.logger.critical("can not build gateways %s"%(gatewayName),exc_info=True)
-                self.gatewaysCFG[gatewayName]=config
                 self.gatewaysInstance[gatewayName]['enable']=False
-                
                 raise Exception
         else:
             self.logger.info("gateways %s is not on this host"%(gatewayName))
-            self.gatewaysCFG[gatewayName]=config 
             
     def startGateway(self,gatewayName):
+        '''
+        start a gateway, the gateway have to build before
+        
+        raise a exception
+        '''
+        if gatewayName not in self.gatewaysInstance:
+            self.logger.error("gateways %s is not excisting"%(gatewayName))
+            raise Exception
         try:
             self.gatewaysInstance[gatewayName]['instance'].start()
             self.gatewaysInstance[gatewayName]['status']="start"
         except:
-            self.logger.error("can not start gateways%s "%(gatewayName),exc_info=True)
+            self.logger.error("can not start gateways %s"%(gatewayName),exc_info=True)
             self.gatewaysInstance[gatewayName]['status']="stop"
             self.gatewaysInstance[gatewayName]['enable']=False
+            raise Exception
+        
     def shutdownAllGateways(self):
         '''
         shutdown all gateways
