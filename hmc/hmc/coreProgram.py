a260f8be77cc2c4589c70d9eb3726fe476d047bb
uschoen <uschoen@DESKTOP-MK4S8D3.johjoh.intern>
2018-01-20 20:55:43 +0100

    change coreProgram

--- hmc/hmc/coreProgram.py
+++ hmc/hmc/coreProgram.py
@@ -4,6 +4,7 @@ Created on 16.01.2018
 @author: uschoen
 '''
 
+
 class coreProgram:
     '''
     classdocs
@@ -16,6 +17,16 @@ class coreProgram:
     
     def restoreProgram(self):
         pass
-    
 
+    def runProgram(self,deviceID,channelName,eventTyp,programName):
+        if programName not in self.program:
+            self.logger.error("can not fnd programName %s"%programName)
+            return
+        try:
+            program=self.program.get(programName)
+            self.programPraraphser.praraphseProgramm(self)
+        except:
+            self.logger.error("can not strat program %s"%programName)
+
+    
     
