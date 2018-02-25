18f14dc9831c0fdf5ff383030c82c8a9c5bbca6d
uschoen <uschoen@Laptop-gross.johjoh.intern>
2018-02-25 22:38:03 +0100

    rebuild corClient. Remove Core Server and move to new Gateway (hmc)

--- hmc/hmc/coreClients.py
+++ hmc/hmc/coreClients.py
@@ -7,11 +7,10 @@ __version__ = "3.0"
 
 
 
-from hmc import socketServer
 from hmc import socketClient
 import threading
 
-class coreConnector(object):
+class coreClients(object):
     
     def updateCoreClient(self,coreName,coreCFG,forceUpdate=False):
         '''
@@ -31,50 +30,45 @@ class coreConnector(object):
         '''
         try:
             self.logger.info("try to update core sync server")
-            if coreName in self.coreClientsCFG:
-                if self.coreClientsCFG.get(coreName)==coreCFG:
-                    self.logger.info("%s core have same config, nothing to do"%(coreName))
-                    return
-                if coreCFG.get('enable',False):
-                    threading.Thread(target=self.__stopAndDeleteClient,args = (coreName,coreCFG,forceUpdate)).start()
+            threading.Thread(target=self.__updateClient,args = (coreName,coreCFG,forceUpdate)).start()
         except:
             self.logger.error("can not update core sync server",exc_info=True)
-            raise     
-    
-    def __stopAndDeleteClient(self,coreName,coreCFG,forceUpdate=False):
+            raise 
+        
+    def __updateClient(self,coreName,coreCFG,forceUpdate=False):
         try:
+            if self.coreClientsCFG.get(coreName,False)==coreCFG:
+                self.logger.info("%s core have same config, nothing to do"%(coreName))
+                return
             syncStatus=False
-            if  self.eventHome(coreName):
-                '''
-                coreServer
-                '''
-                self.deleteCoreClient(coreName,forceUpdate)  
-            else:
-                '''
-                coreClient
-                '''
-                syncStatus=self.CoreClientsConnections[coreName].getSyncStatus()
-                self.CoreClientsConnections[coreName].busyoutClient()
-                self.deleteCoreClient(coreName,forceUpdate)   
-            self.__buildCoreClient(coreName,coreCFG,syncStatus)
-            self.updateRemoteCore(forceUpdate,coreName,'updateCoreClient',coreName,coreCFG)
+            if coreName in self.CoreClientsConnections:                    
+                syncStatus=self.CoreClientsConnections[coreName]['instance'].getSyncStatus()
+                self.CoreClientsConnections[coreName]['instance'].busyoutClient()
+            self.__deleteCoreClient(coreName)
+            self.__buildCoreClient(coreName, coreCFG, syncStatus)
+            self.updateRemoteCore(forceUpdate,coreName,'updateCoreClient',coreName,coreCFG)   
         except:
             self.logger.error("can not update remote core %s"%(coreName))    
     
+    def __deleteCoreClient(self,coreName):
+        try:
+            if coreName in self.CoreClientsConnections[coreName]:
+                self.logger.error("delete core Client %s"%(coreName)) 
+                coreClient= self.CoreClientsConnections[coreName]['instance']
+                coreClient.shutdown()
+                del self.CoreClientsConnections[coreName]
+            if coreName in self.coreClientsCFG:
+                del self.coreClientsCFG[coreName]
+        except:
+            self.logger.error("can not delete remote core %s"%(coreName))
+        
     def deleteCoreClient(self,coreName,forceUpdate=False):
         try:
-            self.logger.error("delete core %s"%(coreName)) 
-            if  self.eventHome(coreName):
-                self.ConnectorServer[coreName].shutdown() 
-                del self.ConnectorServer[coreName] 
-            else:
-                self.CoreClientsConnections[coreName].shutdown()
-                del self.CoreClientsConnections[coreName]
-            del self.coreClientsCFG[coreName]
+            self.__deleteCoreClient(coreName) 
             self.updateRemoteCore(forceUpdate,coreName,'deleteCoreClient',coreName)       
         except:
-            self.logger.error("can not delete remote core %s"%(coreName)) 
-    
+            self.logger.error("can not delete remote core %s"%(coreName))
+        
     def restoreCoreClient(self,coreName,args,syncStatus=False):
         '''
         add a core connector as server or client
@@ -90,12 +84,12 @@ class coreConnector(object):
         syncStatus true/false if true it set the status to the core to is sync
         '''
         try:
-            self.__buildCoreClient(coreName,args)
+            self.__buildCoreClient(coreName,args,syncStatus)
         except:
             self.logger.error("can not restore core sync server %s"%(coreName),exc_info=True)
             raise Exception
-    
-    def addCoreClient(self,coreName,args):
+        
+    def addCoreClient(self,coreName,args,forceUpdate=False):
         '''
         add a core connector as server or client
         corename=the corename of the client (name@hostname)
@@ -111,64 +105,69 @@ class coreConnector(object):
         '''
         try:
             self.__buildCoreClient(coreName,args)
-            self.updateRemoteCore(False,coreName,'addCoreClient',args)
+            self.updateRemoteCore(forceUpdate,coreName,'addCoreClient',args)
         except:
-            self.logger.error("can not restore core sync server %s"%(coreName),exc_info=True)
-            raise Exception          
+            self.logger.error("can not add core coreclient %s"%(coreName),exc_info=True)
+            raise Exception             
         
-    def __buildCoreClient(self,coreName,args,syncStatus=False):
+    def __buildCoreClient(self,coreName,coreCFG,syncStatus=False):
         '''
         add a core connector as server or client
         corename=the corename of the client (name@hostname)
-        args:{
-                    "hostName":"raspberry1",
-                    "socketTimeout":8
-                    "enable":true,
+        
+        syncStatus true/false if true it set the status to the core to is sync
+        '''
+        gatewayConfig={
+                    "hostName":self.args['global']['host'],
+                    "socketTimeout":8,
+                    "enable":False,
                     "user":"user1",
                     "password":"password12345",
                     "ip":"127.0.0.1",
                     "port":5090,
                     "timeout":50}
-        syncStatus true/false if true it set the status to the core to is sync
-        '''
-        self.logger.info("try to add core sync server %s"%(coreName))
-        args['hostName']=coreName
+        self.logger.info("try to add core client %s"%(coreName))
+        gatewayConfig.update(coreCFG)
         try:
-            if args['enable']==True:
+            if gatewayConfig['enable']==True:
                 if self.eventHome(coreName):
-                    '''
-                    start core Server
-                    '''
-                    self.logger.info("core %s is sync server "%(coreName))
-        
-                    self.ConnectorServer[coreName]=socketServer.server(args,self)
-                    self.ConnectorServer[coreName].daemon=True
-                    self.ConnectorServer[coreName].start()        
+                    self.logger.info("core client %s is this server, ignore"%(coreName))      
                 else:
                     '''
                     start core Client
                     '''
-                    self.logger.info("core %s is sync client "%(coreName))  
-                    self.CoreClientsConnections[coreName]=socketClient.CoreConnection(args,self)
-                    if syncStatus:
-                        self.CoreClientsConnections[coreName].setSyncStatus(syncStatus)
-                    self.CoreClientsConnections[coreName].daemon=True
-                    self.CoreClientsConnections[coreName].start()   
+                    self.logger.info("core client %s is build"%(coreName))
+                    coreClient={
+                        "instance":None,
+                        "running":False, 
+                        "syncStatus":syncStatus
+                        } 
+                    coreClient['instance']=socketClient.CoreConnection(gatewayConfig,self)
+                    if coreClient['syncStatus']:
+                        coreClient['instance'].setSyncStatus(syncStatus)
+                    coreClient['instance'].daemon=True
+                    coreClient['instance'].start() 
+                    coreClient['running']=True
+                    self.CoreClientsConnections[coreName]=coreClient
+                    
             else:
-                self.logger.info("core Client %s is disable"%(args['hostName']))  
+                self.logger.info("core Client %s is disable"%(gatewayConfig['hostName']))  
         except:
-            self.logger.error("can not start core",exc_info=True)
-        self.coreClientsCFG[coreName]=args  
+            self.logger.error("can not start core Client",exc_info=True)
+        self.coreClientsCFG[coreName]=gatewayConfig  
         
-        
-    def updateRemoteCore(self,force,deviceID,calling,*args): 
-        if not force:
+    def updateRemoteCore(self,forceUpdate,deviceID,calling,*args): 
+        if not forceUpdate:
             if not self.eventHome(deviceID):
                 return           
         for coreName in self.CoreClientsConnections:
             try:
-                self.CoreClientsConnections[coreName].updateCore(deviceID,calling,args)
-                self.logger.debug("store in remote Core queue: %s success"%(coreName))
+                if self.CoreClientsConnections[coreName]['running']:
+                    coreClient=self.CoreClientsConnections[coreName]['instance']
+                    coreClient.updateCore(deviceID,calling,args)
+                    self.logger.debug("store in remote Core queue: %s success"%(coreName))
+                else:
+                    self.logger.error("core client %s is not running"%(coreName))
             except:
-                self.logger.error("can not store remote Core queue: %s"%(coreName),exc_info=True)          
+                self.logger.error("can not update core Client queue: %s"%(coreName),exc_info=True)          
 
